574d22a0d2d209630f9e38b411d52a29e5e22047
Anurag Singh <sagacious112358@gmail.com>
2026-03-10 20:54:47 +0100

    Fix include directive for pybind11/complex.h

--- CPP/fermionic_operator_py.cpp
+++ CPP/fermionic_operator_py.cpp
@@ -2,7 +2,7 @@
 #include <pybind11/stl.h>
 #include <pybind11/operators.h>
 #include <pybind11/functional.h>
-#include <pybind11/complex.h>
+#include <pybind11/complex.h> 
 #include <sstream>
 #include <iomanip>
 
